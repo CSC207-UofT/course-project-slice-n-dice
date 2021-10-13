cb99bdca7f7753ffe69e681d8de1419e5e213051
rahulj2001 <46646243+rahulj2001@users.noreply.github.com>
2021-10-13 18:34:06 -0400

    Update Schedulable.java

--- src/main/Schedulable.java
+++ src/main/Schedulable.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 interface Schedulable {
     public ArrayList<String> getSchedule();   //String list of times that the player is available in the week.
     public String getUsername();
