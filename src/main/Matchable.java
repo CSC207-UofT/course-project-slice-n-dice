4f2a3e6c7c2ec2e05f9bc16a95ef72405291b0da
rahulj2001 <46646243+rahulj2001@users.noreply.github.com>
2021-10-13 18:34:47 -0400

    Update Matchable.java

--- src/main/Matchable.java
+++ src/main/Matchable.java
@@ -1,5 +1,8 @@
+import java.util.ArrayList;
+
 public interface Matchable {
     public double getSkill();
     public double[] getLocation();
     public String getUsername();
+    public ArrayList<String> getSwipedUsers();
 }
