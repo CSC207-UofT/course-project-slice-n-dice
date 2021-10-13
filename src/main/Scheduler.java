71bc837fbd97d474d7aaa7bc027c5de2521d54a0
Seung Ho Park <seungho.park@mail.utoronto.ca>
2021-10-13 15:49:17 -0400

    Removed constructor parameters and have methods take in the manageable objects instead.

--- src/main/Scheduler.java
+++ src/main/Scheduler.java
@@ -1,29 +1,19 @@
-public class Scheduler implements Manageable{
-
-    private Schedulable player1;
-    private Schedulable player2;
-    private ArrayList<String> p1times;
-    private ArrayList<String> p2times;
+public class Scheduler{
 
     /**
-     * Creates a new Scheduler object. This constructor takes in 2 schedulable objects as arguments.
-     * @param player1 a schedulable object with available times.
-     * @param player2 another schedulable object with available times.
+     * Creates a new Scheduler object.
      */
-    public Scheduler(Schedulable player1, Schedulable player2){
-        this.player1 = player1;
-        this.player2 = player2;
-        this.p1times = player1.get_schedule();
-        this.p2times = player2.get_schedule();
+    public Scheduler(){
     }
 
     /**
      * This method returns a list of common times between two schedulable objects (Tennis players).
+     * How to call -> Scheduler.getCommonTimes((Schedulable) p1, (Schedulable) p2)
      */
-    public ArrayList<String> getCommonTimes(){
+    public ArrayList<String> getCommonTimes(Schedulable player1, Schedulable player2){
         ArrayList<String> commonTimes = new ArrayList<>;
-         for (String item1: p1times){
-             for (String item2: p2times){
+         for (String item1: player1.get_schedule()){
+             for (String item2: player2.get_schedule()){
                  if (item1.equals(item2)){
                      commonTimes.add(item1);
                  }
@@ -32,8 +22,13 @@ public class Scheduler implements Manageable{
          return commonTimes;
     }
 
-    public ArrayList<String> getSchedulableUsername(){
-        username_pair = [player1.getUsername, player2.getUsername]
+    /**
+     * This method returns a list of the two schedulable objects (Tennis players).
+     * How to call -> Scheduler.getSchedulableUsernames((Schedulable) p1, (Schedulable) p2)
+     */
+    public ArrayList<String> getSchedulableUsernames(Schedulable player1, Schedulable player2){
+        String[] username_pair = {player1.getUsername(), player2.getUsername()};
+        return username_pair;
     }
 }
 
