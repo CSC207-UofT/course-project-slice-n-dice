2bdb479fe1682aaccf973ff25b8f3bd31c315346
Aman Rana <aman.rana@mail.utoronto.ca>
2021-10-13 15:38:39 -0400

    Updates to MatchMaker, adding comments and cleaning code

--- src/main/MatchMaker.java
+++ src/main/MatchMaker.java
@@ -12,6 +12,14 @@ public class matchMaker implements Manageable {
         this.player2 = player2;
     }
 
+    /**
+     * setScore method returns a weighted average which exemplifies the match suitability between two players
+     * a score of 0 means the match is perfect, the two players are in the same location and have the same skill
+     * levels. The score is a weighted average which eighs location as twice as important as skill level difference.
+     * @param score
+     * @param player1
+     * @param player2
+     */
     public void setScore(double score, Matchable player1, Matchable player2) {
         double skill_difference;
         double location_difference;
@@ -20,18 +28,23 @@ public class matchMaker implements Manageable {
         skill_difference = Math.abs(player1.getSkill() - player2.getSkill());
         location_difference = sqrt(Math.pow((player1.getLocation()[0] - player2.getLocation()[0]), 2) +
                 Math.pow((player1.getLocation()[1] - player2.getLocation()[1]), 2));
-        this.score = (location_difference * 0.5 + skill_difference) / 2; // sets a score of match suitability which weighs distance over skill
+        this.score = (location_difference * 0.5 + skill_difference) / 2; // calculates the weighted average
     }
 
     public double getScore() {
         return score;
     }
 
+    /**
+     * setUsernames sets the usernames attribute to an array list of the players usernames
+     * @param player1
+     * @param player2
+     */
     public void setUsernames(Matchable player1, Matchable player2) {
         usernames = [player1.getUsername(), player2.getUsername()]
     }
 
-    public ArrayList<String> getUsernames() {
+    public ArrayList<String> getMatchMakerUsernames() {
         return usernames;
     }
 }
