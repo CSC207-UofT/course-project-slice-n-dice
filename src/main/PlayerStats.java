ad05566b9a2b5028bf825db7ca8947801e12e049
Seung Ho Park <seungho.park@mail.utoronto.ca>
2021-12-08 13:18:53 -0500

    Made changes to the isReadReady method and made loadandsaveJSON as a helper function for isReadReady.

--- src/main/PlayerStats.java
+++ src/main/PlayerStats.java
@@ -1,4 +1,7 @@
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 import java.io.*;
 import java.nio.file.Files;
@@ -18,7 +21,7 @@ public class PlayerStats {
                 = Instant.parse("0000-00-00000:00:00.000");
         this.time1 = FileTime.from(instant);
     }
-
+    /*
     public int numGamesPlayed(StatsMaker player1) {
         return player1.getWinsandLosses()[0] + player1.getWinsandLosses()[1];
     }
@@ -29,6 +32,73 @@ public class PlayerStats {
 
     public double getWinLossRatio(StatsMaker player1) {
         return (double) player1.getWinsandLosses()[0] / player1.getWinsandLosses()[1];
+    }
+     */
+
+    public int numGamesPlayed(int wins, int losses){
+        return wins + losses;
+    }
+
+    public double getWinRate(int wins, int losses){
+        return (double) wins / (wins + losses);
+    }
+
+    public double getWinLossRatio(int wins, int losses) {
+        return (double) wins / losses;
+    }
+
+    public void isReadReady(String path, String path2){
+        while (! checkNewChange(path)) {
+            File f = new File(path);
+            time1 = getLastModifiedTime(f.getName());
+            JSONParser parser = new JSONParser();
+            try {
+                var obj = parser.parse(new FileReader(path));
+                JSONObject jsonObject =  (JSONObject) obj;
+                String wins =  (String) jsonObject.get("wins");
+                String losses = (String) jsonObject.get("losses");
+
+                int new_wins = Integer.parseInt(wins);
+                int new_losses = Integer.parseInt(losses);
+
+                loadandsaveJSON(path2, new_wins, new_losses);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        }
+        // YOU ARE READING FROM statspipe1.json
+        // you have an instance variable that stores the last time the file was modified (initially, set it to 0)
+        // have a while loop that is constantly checking the last updated time of the file
+        // google how to access a file's metadata in Java/what the variable would be called
+        // if the last updated time is newer, do these two things
+        // 1. set the new modified time to the one you just read (NOTE: make sure you're saving the last UPDATED time,
+        // not the time this is happening)
+        // 2. break out of the while loop and call the method that reads the json information
+    }
+
+    public void loadandsaveJSON(String path, int wins, int losses) {
+        double win_rate = getWinRate(wins, losses);
+        double winLossRatio = getWinLossRatio(wins, losses);
+        int gamesPlayed = wins + losses;
+
+        JSONObject jsonObject = new JSONObject();
+        //Inserting key-value pairs into the json object
+        jsonObject.put("gamesPlayed", String.valueOf(gamesPlayed));
+        jsonObject.put("winRate", String.valueOf(win_rate));
+        jsonObject.put("winLossRatio", String.valueOf(winLossRatio));
+        try {
+            FileWriter file = new FileWriter(path);
+            file.write(jsonObject.toJSONString());
+            file.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        // just parse the json information into two variables: wins and losses
+        // call the methods that manipulate that information (like get win rate, whatever) and save those to variables
+        // create a new JSON object with these three variables
+        // Save that JSON object to statspipe2.json
+
     }
 
     public boolean readJson(String filepath, JSONObject jsonObject) {
@@ -60,11 +130,7 @@ public class PlayerStats {
     public boolean checkNewChange(String filepath){
         File f = new File(filepath);
         FileTime xyz = getLastModifiedTime(f.getName());
-        if (xyz.equals(time1)) {
-            return false;
-        }
-        else
-            return true;
+        return !xyz.equals(time1);
     }
 
     public FileTime getLastModifiedTime(String filepath){
@@ -80,12 +146,6 @@ public class PlayerStats {
         }
     return null;
     }
-
-//    public boolean sendStats(String filepath){
-//        while (checkNewChange(filepath)) {
-//
-//        }
-//    }
 }
 
 
