f107050abdf200d76b5ced889d84a8cd876cb718
Seung Ho Park <seungho.park@mail.utoronto.ca>
2021-12-08 13:26:01 -0500

    Created a PlayerStatsTest class

---
+++ tests/PlayerStatsTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlayerStatsTest {
+    private final String path = "statspipe2.json";
+    private final String path2 = "statspipe1.json";
+
+    @Test
+}
